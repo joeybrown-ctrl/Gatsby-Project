0506e53f135ebf5e306da3dce3c4c957ee8c6f29
Joey Brown <gjoey.brown@gmail.com>
2021-02-15 17:16:50 -0800

    debugged each page after testing

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Layout from '../components/Layout';
 
 const ContactPage = () => {
     return (
