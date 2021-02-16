67830d446645c091db107b84495c9c714c650123
Joey Brown <gjoey.brown@gmail.com>
2021-02-15 17:37:23 -0800

    pulled in styles into index.scss

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
-import '../styles/index.css';
+import '../styles/index.scss';
 
 const Layout = (props) => {
     return(
