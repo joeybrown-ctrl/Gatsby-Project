4cb0cfe4617bd466ffa9859fbe5af1a16f2c3cf8
Joey Brown <gjoey.brown@gmail.com>
2021-02-25 18:26:52 -0800

    added query for createPages api  within export on gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,7 +22,43 @@ module.exports.onCreateNode = ({ node, actions }) => {
             value: slug
         });
     }
+}
 
+module.exports.createPages = async ({ graphql, actions }) => {
+
+    const { createPage } = actions;
+    const blogTemplate = path.resolve('./src/templates/blog.js');
+
+    // this graphql does not reference the graphql api on the front end. This graphql is a function and it's calling our query.
+
+    const response = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        }
+    
+    `)
+
+    response.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    })
+
+    //1. get path to template
+    //2. get markdown data
+    //3. create new pages
 
 
 }
