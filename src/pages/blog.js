03207a63a971dfa5b3864ed204682df68d7a37e0
Joey Brown <gjoey.brown@gmail.com>
2021-03-06 17:11:40 -0800

    created css module for blog and added styling.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import blogStyles from './blog.module.scss';
 
 // 1. fetch the slug for post
 // 2. use slug to generate a link to the post's page
@@ -30,10 +31,10 @@ const BlogPage = () => {
     return (
         <Layout>
             <h1>Blog</h1>
-            <ol>
+            <ol className={blogStyles.posts}>
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
-                        <li>
+                        <li className={blogStyles.post}>
                             <Link to={`/blog/${edge.node.fields.slug}`}>
                                 <h2>{edge.node.frontmatter.title}</h2>
                                 <p>{edge.node.frontmatter.date}</p>
